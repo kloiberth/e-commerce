3dbf5c4ff89c615b608b12f0560a819898633f84
Kloiberth Garcia <kloiberthgarcia0902@gmail.com>
2022-11-29 13:51:10 -0500

    product detail

--- src/pages/ProductsDetails.jsx
+++ src/pages/ProductsDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { Link, Route, useParams } from 'react-router-dom';
 import { getProductsThunk } from '../store/slice/products.slice';
 
 const ProductsDetails = () => {
 
-    const {id} = useParams();
+    const { id } = useParams();
     const dispatch = useDispatch();
     const productsDetail = useSelector(state => state.products);
     const productFound = productsDetail.find(product => product.id === Number(id));
@@ -19,14 +20,46 @@ const ProductsDetails = () => {
 
     return (
         <div>
-            <h2>ruta de productos</h2>
-            {productFound?.title}
-            <img src={productFound?.productImgs[0]} alt={productFound?.title} />
+            <Row>
+                <Col lg={6}>
+                    <div className='product-detail'>
+                        <h2>{productFound?.title}</h2>
+                        <img className='product__img' src={productFound?.productImgs[0]} alt={productFound?.title} />
+                    </div>
+                </Col>
+                <Col lg={6}>
+                    <div className='card__info'>
+                        <h2>{productFound?.title}</h2>
+                        <p> {productFound?.description}</p>
 
-            <h3>product filter</h3>
-            {relateProduct.map(product => (
-                <li>{product.title}</li>
-            ))}
+                    </div>
+                </Col>
+
+
+            </Row>
+            <Row>
+                <div className='items-card'>
+                    <h3>Discover similar items</h3>
+                    <Row xs={1} md={2} lg={3} className="g-4">
+                        {relateProduct.map(product => (
+                            <Col key={product.id}>
+                                <Card>
+                                    <Link className='card__link' to={`/product/${product.id}`}>
+                                        <Card.Img className='card__img' variant="top" src={product.productImgs[0]} alt={product.title} />
+                                        <Card.Body>
+                                            <Card.Title>{product.title}</Card.Title>
+                                            <Card.Text>
+                                                {product.price}
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Link>
+                                </Card>
+
+                            </Col>
+                        ))}
+                    </Row>
+                </div>
+            </Row>
         </div>
     );
 };
